4cb416f048b32988bc2a5021ef34646b338655ca
LMD <lisa.dratva@gmail.com>
2023-04-28 10:26:59 +0100

    Float support

--- tcr_epitope/adapters/utils.py
+++ tcr_epitope/adapters/utils.py
@@ -5,4 +5,7 @@ def validate_peptide_sequence(seq: str) -> bool:
     """
     Checks if a given sequence is a valid peptide sequence.
     """
-    return all([aa in AMINO_ACIDS for aa in seq])
+    if isinstance(seq, str):
+        return all([aa in AMINO_ACIDS for aa in seq])
+    else:
+        return False
