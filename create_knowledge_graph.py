3e8ef7edc41a9da59318f7a8d586c1a0ffb4ad1d
Lera <vd.dragan21@gmail.com>
2025-07-16 13:51:52 +0200

    Update create_knowledge graph

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -1,13 +1,25 @@
+"""
+This script creates a knowledge graph from various immunological databases
+with receptor-epitope matching information and saves it in JSON format.
+"""
+
 from argparse import ArgumentParser
 
 from biocypher import BioCypher
 
+from tcr_epitope.adapters.cedar_adapter import CEDARAdapter
+from tcr_epitope.adapters.iedb_adapter import IEDBAdapter
 from tcr_epitope.adapters.mcpas_adapter import MCPASAdapter
+from tcr_epitope.adapters.neotcr_adapter import NeoTCRAdapter
+from tcr_epitope.adapters.tcr3d_adapter import TCR3DAdapter
+from tcr_epitope.adapters.trait_adapter import TRAITAdapter
+from tcr_epitope.adapters.utils import save_airr_cells_json
 from tcr_epitope.adapters.vdjdb_adapter import VDJDBAdapter
 
 parser = ArgumentParser()
 parser.add_argument("--test", default=False)
 parser.add_argument("--cache_dir", default=None)
+parser.add_argument("--species_names", default=None, help="Path to save unique species names as csv")
 
 args = parser.parse_args()
 
@@ -16,7 +28,13 @@ bc = BioCypher(cache_directory=args.cache_dir)
 adapters = [
     VDJDBAdapter(bc, args.test),
     MCPASAdapter(bc, args.test),
-    # IEDBAdapter(bc, **kwargs),
+    TRAITAdapter(bc, args.test),
+    IEDBAdapter(bc, args.test),
+    VDJDBAdapter(bc, args.test),
+    MCPASAdapter(bc, args.test),
+    TCR3DAdapter(bc, args.test),
+    NeoTCRAdapter(bc, args.test),
+    CEDARAdapter(bc, args.test),
 ]
 
 for adapter in adapters:
@@ -24,3 +42,9 @@ for adapter in adapters:
     bc.add(adapter.get_edges())
 
 airr_cells = bc.get_kg()
+
+# This step required the final kg to be in the airr format (dbms specified in the biocypher config)
+save_airr_cells_json(airr_cells, args.cache_dir)
+
+# Usage example:
+# python3 create_knowledge_graph.py --test True --cache_dir ./cache
