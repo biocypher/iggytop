c360d7c9b6a084955d4e8c9517123e9e3ef4713c
Raphael De Gottardi <dego@zhaw.ch>
2026-02-11 14:21:43 +0100

    remove "paired" attribute. can be computed with ir.chain_qc or easily inferred

--- src/iggytop/adapters/base_adapter.py
+++ src/iggytop/adapters/base_adapter.py
@@ -159,11 +159,7 @@ class BaseAdapter(ABC):
                     val = getattr(row, f, None)
                     if val is not None and not pd.isnull(val):
                         cell[f] = val
-
-                if not pd.isnull(c1_cdr3) and not pd.isnull(c2_cdr3): # This property could be moved into the node representation (depending on the schema)
-                    cell["paired"] = True
-                else:
-                    cell["paired"] = False
+                        
                 self._airr_cells.append(cell)
 
         return self._airr_cells
