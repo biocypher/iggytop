0e110962db2e987c0f9546b75f67a7844f48901f
Raphael De Gottardi <raphael.degottardi@outlook.com>
2026-03-09 10:52:51 +0100

    Exclude receptors/epitopes without relationship

--- src/iggytop/adapters/base_adapter.py
+++ src/iggytop/adapters/base_adapter.py
@@ -70,8 +70,15 @@ class BaseAdapter(ABC):
         """
         if self._table is None:
             self._table = self.read_table(self._bc, self._table_path, self._receptors, self._test)
-            self._table = self._table[~(self._table["PMID"] == "no_pmid_1036521") & ~self._table["PMID"].astype(str).str.contains("https://www.10xgenomics.com", na=False)] 
             # Filter out 10X Genomics dataset as it has been critisized for poor confidence.
+            self._table = self._table[~(self._table["PMID"] == "no_pmid_1036521") & ~self._table["PMID"].astype(str).str.contains("https://www.10xgenomics.com", na=False)] 
+
+            # Filter out entries without receptor information (both chains missing) or without epitope information
+            self._table = self._table[
+                ~(self._table[REGISTRY_KEYS.CHAIN_1_CDR3_KEY].isna() & self._table[REGISTRY_KEYS.CHAIN_2_CDR3_KEY].isna()) &
+                ~self._table[REGISTRY_KEYS.EPITOPE_KEY].isna()
+            ]
+            
         return self._table
     
     @property
